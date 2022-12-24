ddb12484fd65b8fefe4491fc4dab30fbc3f4bfca
chaipawn <chaiyapawn.n@gmail.com>
2022-12-24 18:50:52 +0700

    update domain expense

--- domain/expense.go
+++ domain/expense.go
@@ -8,12 +8,48 @@ type Expense struct {
 	tags   ExpenseTags
 }
 
-func NewExpense(id int, title string, amount float64, note string, tags []string) Expense {
+func NewExpense(id ExpenseId, title ExpenseTitle, amount ExpenseAmount, note ExpenseNote, tags ExpenseTags) Expense {
 	return Expense{
-		id:     NewExpenseId(id),
-		title:  NewExpenseTitle(title),
-		amount: NewExpenseAmount(amount),
-		note:   NewExpenseNote(note),
-		tags:   NewExpenseTags(tags...),
+		id:     id,
+		title:  title,
+		amount: amount,
+		note:   note,
+		tags:   tags,
 	}
+}
+
+func (expense Expense) Id() ExpenseId {
+	return expense.id
+}
+
+func (expense Expense) Title() ExpenseTitle {
+	return expense.title
+}
+
+func (expense Expense) Amount() ExpenseAmount {
+	return expense.amount
+}
+
+func (expense Expense) Note() ExpenseNote {
+	return expense.note
+}
+
+func (expense Expense) Tags() ExpenseTags {
+	return expense.tags
+}
+
+func (expense Expense) SetTitle(title ExpenseTitle) Expense {
+	return NewExpense(expense.id, title, expense.amount, expense.note, expense.tags)
+}
+
+func (expense Expense) SetAmount(amount ExpenseAmount) Expense {
+	return NewExpense(expense.id, expense.title, amount, expense.note, expense.tags)
+}
+
+func (expense Expense) SetNote(note ExpenseNote) Expense {
+	return NewExpense(expense.id, expense.title, expense.amount, note, expense.tags)
+}
+
+func (expense Expense) SetTags(tags ExpenseTags) Expense {
+	return NewExpense(expense.id, expense.title, expense.amount, expense.note, tags)
 }
