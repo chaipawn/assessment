574cacb6dff71bd7552b21f5a9c97937a7bdecc1
chaipawn <chaiyapawn.n@gmail.com>
2022-12-25 14:41:14 +0700

    setup api server

--- server.go
+++ server.go
@@ -1,11 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"database/sql"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
+
+	"github.com/chaipawn/assessment/webapi"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS expenses (
+		id SERIAL PRIMARY KEY,
+		title TEXT,
+		amount FLOAT,
+		note TEXT,
+		tags TEXT[]
+	);`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app := webapi.NewExpenseAPI(os.Getenv("PORT"), db)
+	go func() {
+		if err = app.Start(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("http server graceful shutdown...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err = app.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
 }
