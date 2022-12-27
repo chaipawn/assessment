f6997768ab24587aaeafddd0d5c158b687ddf2ac
chaipawn <chaiyapawn.n@gmail.com>
2022-12-27 20:22:25 +0700

    add function update expense in application layer

--- application/persistence/expense_repository.go
+++ application/persistence/expense_repository.go
@@ -9,3 +9,8 @@ type AddExpenseCommandRepository interface {
 type GetExpenseQueryRepository interface {
 	Read(id domain.ExpenseId) (*domain.Expense, error)
 }
+
+type UpdateExpenseCommandRepository interface {
+	Read(id domain.ExpenseId) (*domain.Expense, error)
+	Update(expense domain.Expense) (*domain.Expense, error)
+}
